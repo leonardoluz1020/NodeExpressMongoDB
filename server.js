04b754c852d3070f64e6bb626c43638aa02292e5
Leonardo Luz <leonardoluz10@hotmail.com>
2022-12-15 11:20:30 -0300

    Criando modulos e refatorando projeto

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 import http from 'http' // importando o modulo http que já vem no node
 const port = 3000; // criando uma const port para colocarmos o endereço da  porta que será usada para um servidor local
 // criando o servidor e definir o que ele vai escutar e como vai acontecer essa comunicação 
+
+const rotas = {
+    '/':'Curso de node',
+    '/livros': 'Entrei na pag de livros',
+    '/autores': 'Listagem de autores'
+}
+
 const server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-type': 'text/plain'}); // resposta de cabeçalho com statusCode 200 e tipo de conteudo (Content-type) no caso enviadmos  texto (text/plain)
-    res.end('Curso de Node');// finalizando resposta mandando o texto que será enviado 
+    res.end(rotas[req.url]);// finalizando resposta mandando o texto que será enviado 
 })
 // definindo a porta que o servidor vai escutar do link que foi criado
 server.listen(port, () => {
