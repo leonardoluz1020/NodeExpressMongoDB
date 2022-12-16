b2d6bad9c2bfa061d729fc4f1580d4dce63052c6
Leonardo Luz <leonardoluz10@hotmail.com>
2022-12-16 15:32:12 -0300

    Usando o app2 para subir no postman

--- src/app2.js
+++ src/app2.js
@@ -1,38 +1,49 @@
-// import express from 'express'
+import express from 'express'
 
-// const app = express();
+const app = express();
+app.use(express.json())
 
-// app.use(express.json())
+const livros = [
+    { id: 0, "titulo": "Eu sou a lenda" },
+    { id: 1, "titulo": "Passageiros" },
+    { id: 2, "titulo": "Wednesday"},
+]
 
-// const livros = [
-//     {id: 1, "titulo": "Senhor dos Aneis" },
-//     {id: 2, "titulo": "O hobbit"},
-//     {id: 3, "titulo": "star trek"},
-//     {id: 4, "titulo": "passageiros"},
-// ]
+app.get('/',(req, res) => {
+    res.status(200).send('Curso de Node ALURA')
+})
 
-// app.get('/',(req, res) => {
-//     res.status(200).send('Curso de Node');
-// })
-// app.get('/livros',(req, res) => {
-//     res.status(200).json(livros);
-// })
-// app.get('/livros/:id',(req, res) => {
-//     let index = buscaLivro(req.params.id);
-//     res.status(200).json(livros[index])
-// })
-// app.post('/livros',(req, res) => {
-//     livros.push(req.body)
-//     res.status(201).send('Livro acrescentado com sucesso')
-// })
-// app.put('/livros/:id',(req, res) => {
-//     let index = buscaLivro(req.params.id)
-//     livros[index].titulo = req.body.titulo;
-//     res.json(livros)
-// })
+app.get('/livros',(req, res) => {
+    res.status(200).json(livros)
+})
 
-// function buscaLivro(id){
-//     return livros.findIndex(livro => livro.id == id);
-// }
+app.get('/livros/:id',(req, res) => {
+    let {id} = req.params
+    let index = buscaLivro(id)
+    res.status(200).json(livros[index])
+})
 
-// export default app;
+app.post('/livros', (req, res) => {
+    livros.push(req.body)
+    res.status(201).send('Livro cadastrado com sucesso!.')
+})
+
+app.put('/livros/:id', (req, res) => {
+    let {id} = req.params
+    let index = buscaLivro(id)
+    livros[index].titulo = req.body.titulo
+    res.send('Titulo modificado com sucesso!.')
+})
+
+app.delete('/livros/:id', (req, res) => {
+    let {id} = req.params;
+    let index = buscaLivro(id);
+    livros.splice(index,1);
+    res.send('Livro excluido com sucesso');
+})
+
+function buscaLivro(id){
+    return livros.findIndex(livro => livro.id == id);
+}
+
+export default app;
