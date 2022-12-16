93d3876799747ac546a2a1204bc7cad23f1594a6
Leonardo Luz <leonardoluz10@hotmail.com>
2022-12-16 12:20:59 -0300

    criando o put para modificar um objeto

--- src/app.js
+++ src/app.js
@@ -12,7 +12,6 @@ const livros = [
     {id: 3, "titulo": "star trek"},
     {id: 4, "titulo": "passageiros"},
 ]
-
 // criando uma rota usando o metodo get status 200 passando Curso de Node
 app.get('/',(req, res) => {
     res.status(200).send('Curso de Node');
@@ -21,9 +20,27 @@ app.get('/',(req, res) => {
 app.get('/livros',(req, res) => {
     res.status(200).json(livros)
 })
+// criando rota usando o metodo get status 200 passando um objeto pelo id em formato json
+app.get('/livros/:id',(req, res) => {
+    let index = buscarLivros(req.params.id);
+    res.json(livros[index]);
+})
 // craindo uma rota metodo post para cadastrar ou criar novos objetos
 app.post('/livros',(req, res) => {
     livros.push(req.body);
     res.status(201).send('Livro cadastrado com sucesso')
 })
+// Modificando um objeto pelo seu id usando o metodo put
+app.put('/livros/:id',(req, res) => {
+    let index = buscarLivros(req.params.id);
+    livros[index].titulo = req.body.titulo;
+    res.json(livros);
+})
+// função para buscar indice da array de objetos usando o metodo findIndex
+function buscarLivros(id){
+    return  livros.findIndex(livro => livro.id == id);
+}
+
+
+
 export default app; // exportando a const app 
