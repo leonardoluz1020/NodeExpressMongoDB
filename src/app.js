3c6c82fe8d7a4eae70ade10d7baab823d4084dd8
Leonardo Luz <leonardoluz10@hotmail.com>
2022-12-20 17:05:25 -0300

    Criando o padrão MVC de arquitetura

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import express from "express";// importando o express para usar o app.get
 import db from "./config/dbConnect.js";// importando a conexão para o app
+import livros from "./models/Livro.js";
+import routes from "./routes/index.js";
 
 // acessando as conexão do projeto passando o db.on para mostrar se algo ocorrer errado
 db.on("error", console.log.bind(console, 'Erro de conexão'))
@@ -14,21 +16,8 @@ const app = express();
 // Usamos o express.json para indicar que vamos usar no post e put o formato json 
 app.use(express.json())
 
-// Criando uma array de objetos com titulos e nome dos filmes
-const livros = [
-    { id: 1, "titulo": "Senhor dos Aneis" },
-    { id: 2, "titulo": "O hobbit" },
-    { id: 3, "titulo": "star trek" },
-    { id: 4, "titulo": "passageiros" },
-]
-// criando uma rota usando o metodo get status 200 passando Curso de Node
-app.get('/', (req, res) => {
-    res.status(200).send('Curso de Node');
-})
-// craindo uma rota usando metodo get status 200 passando a array de livros em formato json
-app.get('/livros', (req, res) => {
-    res.status(200).json(livros)
-})
+routes(app);
+
 // criando rota usando o metodo get status 200 passando um objeto pelo id em formato json
 app.get('/livros/:id', (req, res) => {
     let index = buscarLivros(req.params.id);
